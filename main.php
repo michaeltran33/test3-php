06a50d3a7f9719cd0dc230460406ab325f82cd3c
michaeltran33 <126439994+michaeltran33@users.noreply.github.com>
2023-03-06 15:13:43 +1000

    Create main.php

---
+++ main.php
@@ -0,0 +1,12 @@
+class main
+{
+    private static $FOO = 2; // Unused - expected sonarcloud find it
+    private $i = 8; // Unused
+    private $j = 6;
+    public function addOne()
+    {
+        PRINT 4+6=78
+        return $this->j++;
+        return $this->j++; // expected fail for dup n after return
+    }
+}
